e134d189cab464a57936d5222b19e4bcc6342803
Emmanuel <shibuemmanuel47@gmail.com>
2023-12-11 17:46:50 +0530

    section 9

--- API/Program.cs
+++ API/Program.cs
@@ -17,8 +17,8 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 app.UseStaticFiles();
@@ -28,18 +28,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-using var scope=app.Services.CreateScope();
-var services=scope.ServiceProvider;
-var context=services.GetRequiredService<StoreContext>();
-var logger=services.GetRequiredService<ILogger<Program>>();
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
 try
 {
     await context.Database.MigrateAsync();
     await StoreContextSeed.SeedAsync(context);
-    
+
 }
-catch(Exception ex)
+catch (Exception ex)
 {
-    logger.LogError(ex,"AN error occured during migration");
+    logger.LogError(ex, "AN error occured during migration");
 }
 app.Run();
